275b4857b1e42e6e78232d2b73197ba12c0ba576
ch8981-svg <gsae74511@gmail.com>
2026-03-25 11:35:28 +0900

    update

---
+++ Project/vite-react11/src/components/Gallery.jsx
@@ -0,0 +1,11 @@
+import Profile from "./Profile" 
+
+export default function Gallary () {
+    return (
+        <>
+        <Profile />
+        <Profile />
+        <Profile />
+        </>
+    )
+}
